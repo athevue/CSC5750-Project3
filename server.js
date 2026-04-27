ba8c8eb5087f9b16b348a1bd4fe5d75d889e0ca0
Adam Vue <athevue@gmail.com>
2026-04-26 23:26:58 -0400

    deployment

--- server.js
+++ server.js
@@ -121,6 +121,8 @@ app.get("/students", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
